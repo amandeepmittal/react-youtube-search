270e40b2f057a2c160cd8e689fce9d8ab43aa691
amandeepmittal <amandeepmittal@live.com>
2017-08-26 20:23:47 +0530

    ⚛️ component: searchbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import debounce from 'lodash.debounce';
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyANl999ACEi82UvWilvXMclcow8WbikDKY';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('reactjs');
+	}
+
+	videoSearch(term) {
+		YTSearch(
+			{
+				key: API_KEY,
+				term: term
+			},
+			videos => {
+				this.setState({ videos: videos, selectedVideos: videos[0] });
+			}
+		);
+	}
+
 	render() {
+		const videoSearch = debounce(term => {
+			this.videoSearch(term);
+		}, 300);
 		return (
 			<div className="container">
-				<SearchBar/>
+				<SearchBar onSearchTermChange={videoSearch} />
 			</div>
 		);
 	}
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
 	render() {
 		return (
-			<div className="control search-bar">
-				<h1 className="title is-1">
-					<span className="fa fa-youtube-play" />&nbsp; Youtube InstaSearch
-				</h1>
-				<input
-					className="input"
-					type="text"
-					placeholder="Search a YouTube video..."
-				/>
+			<div className="block">
+				<div className="control search-bar">
+					<h1 className="title is-1">
+						<span className="fa fa-youtube-play" />&nbsp; Youtube InstaSearch
+					</h1>
+					<input
+						className="input"
+						type="text"
+						placeholder="Search a YouTube video..."
+						value={this.state.term}
+						onChange={event => this.onInputChange(event.target.value)}
+					/>
+				</div>
 			</div>
 		);
+	}
+
+	onInputChange(term) {
+		this.setState({ term });
+		this.props.onSearchTermChange(term);
 	}
 }
