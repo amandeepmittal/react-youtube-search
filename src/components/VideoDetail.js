3fa294c118dfeb32d2f7d9657c1cd352dc678188
amandeepmittal <amandeepmittal@live.com>
2017-08-27 22:42:13 +0530

    embed video using responsive iframe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -14,6 +14,19 @@ const VideoDetail = ({video}) => {
       </div>
     );
   }
+
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
+  return (
+    <div className="video-detail columns">
+      <div className="column is-8">
+        <div className="video-container">
+          <iframe title="video_yt" src={url} width="700" height="350"/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
