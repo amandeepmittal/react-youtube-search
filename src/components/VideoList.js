55dd49874aa0d0516404c7be355d3ef65464d5ab
amandeepmittal <amandeepmittal@live.com>
2017-08-28 10:10:42 +0530

    ⚛️ component: VideoListItem

---
+++ src/components/VideoList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoList = props => {
+	const videoItems = props.video.map(video => {
+		return <div>it works</div>;
+	});
+};
+
+export default VideoList;
---
+++ src/components/VideoListItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './VideoListItem';
+
+const VideoListItem = ({ video, selectedVideo }) => {
+	const imageUrl = video.snippet.thumbnail.default.url;
+	const title = video.snippet.title;
+
+	return (
+		<div className="box related-list">
+			<article className="media related-card">
+				<div className="media-left">
+					<figure className="image">
+						<img src={imageUrl} alt="Image" />
+					</figure>
+				</div>
+				<div className="media-content">
+					<div className="content">
+						<p>
+							<span className="video-title">
+								{title}
+							</span>
+						</p>
+					</div>
+				</div>
+			</article>
+		</div>
+	);
+};
+
+export default VideoListItem;
